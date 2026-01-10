46bcbbc5873a33eab1c74da7ddc2adae7d77db92
aee4 <eeyramagbetor@gmail.com>
2026-01-10 20:19:41 +0000

    Add main App component

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BookingProvider } from "./contexts/BookingContext";
+import ScrollToTop from "./components/ScrollToTop";
+import Index from "./pages/Index";
+import Timetable from "./pages/Timetable";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <BookingProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/timetable" element={<Timetable />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </BookingProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
